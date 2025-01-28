3e5013342e77cdb0c178be7da13de99bf32c8b50
Gabriel Fredes <85911230+gabsfredes@users.noreply.github.com>
2025-01-28 13:43:59 -0300

    adjust: view create adjusted to be view edit as well

--- proby/resources/views/create.blade.php
+++ proby/resources/views/create.blade.php
@@ -1,7 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Inserir novo projeto') }}
+            @if (isset($project))
+                Editar projeto
+            @else
+                Inserir novo projeto
+            @endif
         </h2>
     </x-slot>
 
@@ -31,48 +35,61 @@
                         </div>
                     @endif
 
-                    <form name="newProject" id="newProject" method="POST" action="{{ route('projects.store') }}">
-                        @csrf
-                        <div>
-                            <label for="name"
-                                class="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white">Nome do
-                                projeto <span class="text-red-500">*</span></label>
-                            <input type="text" id="name" name="name"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Project BETA" required />
-                        </div>
-                        <div class="mt-4">
-                            <label for="start_date"
-                                class="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white">Data de
-                                início <span class="text-red-500">*</span></label>
-                            <input type="date" id="start_date" name="start_date"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required />
-                        </div>
-                        <div class="mt-4">
-                            <label for="status"
-                                class="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white">Status
-                                <span class="text-red-500">*</span></label>
-                            <select id="status" name="status"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required>
-                                <option value="Pendente">Pendente</option>
-                                <option value="Em Andamento">Em Andamento</option>
-                                <option value="Concluído">Concluído</option>
-                            </select>
-                        </div>
-                        <div class="mt-4">
-                            <label for="description"
-                                class="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white">Descrição</label>
-                            <textarea id="description" name="description"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Descreva o projeto"></textarea>
-                        </div>
-                        <div class="mt-4">
-                            <button type="submit"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Inserir
-                                projeto</button>
-                        </div>
+                    @if (isset($project))
+                        <form action="{{ route('projects.update', $project->id) }}" method="POST">
+                            @method('PUT')
+                        @else
+                            <form name="newProject" id="newProject" method="POST"
+                                action="{{ route('projects.store') }}">
+                    @endif
+                    @csrf
+                    <div>
+                        <label for="name"
+                            class="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white">Nome do
+                            projeto <span class="text-red-500">*</span></label>
+                        <input type="text" id="name" name="name" value="{{ $project->name ?? '' }}"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Project BETA" required />
+                    </div>
+                    <div class="mt-4">
+                        <label for="start_date"
+                            class="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white">Data de
+                            início <span class="text-red-500">*</span></label>
+                        <input type="date" id="start_date" name="start_date" value="{{ $project->start_date ?? '' }}"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required />
+                    </div>
+                    <div class="mt-4">
+                        <label for="status"
+                            class="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white">Status
+                            <span class="text-red-500">*</span></label>
+                        <select id="status" name="status"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required>
+                            <option value="{{ $project->status ?? 'Selecione' }}">
+                                {{ $project->status ?? 'Selecione o status' }}</option>
+                            <option value="Pendente">Pendente</option>
+                            <option value="Em Andamento">Em Andamento</option>
+                            <option value="Concluído">Concluído</option>
+                        </select>
+                    </div>
+                    <div class="mt-4">
+                        <label for="description"
+                            class="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white">Descrição</label>
+                        <textarea id="description" name="description"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Descreva o projeto"></textarea>
+                    </div>
+                    <div class="mt-4">
+                        <button type="submit"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            @if (isset($project))
+                                Atualizar projeto
+                            @else
+                                Inserir projeto
+                            @endif
+                        </button>
+                    </div>
                     </form>
                 </div>
             </div>
