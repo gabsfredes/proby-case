9ef25f10cd11206218de60cd9d2d18d2e32090bb
Gabriel Fredes <85911230+gabsfredes@users.noreply.github.com>
2025-01-28 15:09:17 -0300

    fix: description input at edit form

--- proby/resources/views/create.blade.php
+++ proby/resources/views/create.blade.php
@@ -78,7 +78,7 @@
                             class="block mb-2 text-sm sm:text-lg font-medium text-gray-900 dark:text-white">Descrição</label>
                         <textarea id="description" name="description"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Descreva o projeto"></textarea>
+                            placeholder="Descreva o projeto">{{ $project->description ?? '' }}</textarea>
                     </div>
                     <div class="mt-4">
                         <button type="submit"
