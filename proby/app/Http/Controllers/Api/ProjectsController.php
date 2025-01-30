7b2d8809427b75af4913e8d11d806bc92f3cbcb7
Gabriel Fredes <85911230+gabsfredes@users.noreply.github.com>
2025-01-29 23:33:42 -0300

    feat: show projects api

--- proby/app/Http/Controllers/Api/ProjectsController.php
+++ proby/app/Http/Controllers/Api/ProjectsController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
+
 
 class ProjectsController extends Controller
 {
     public function index() {
+
+        $projects=Project::paginate(5);
+
         return response()->json([
             'status' => true, 
-            'message' => 'Listagem de projetos'], 200);
+            'message' => 'Listagem de projetos, 5 resultados por página',
+            'data' => $projects
+        ], 200);
     }
 }
